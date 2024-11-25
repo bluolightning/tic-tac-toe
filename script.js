3c2f60d9e064c6c9b61d3ea58bf7ad438c83185f
bluolightning <124224350+bluolightning@users.noreply.github.com>
2024-11-25 18:16:08 -0500

    Create players

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const gameboard = {};
 
-
 (function createGameboard() {
     for (let i = 1; i <= 8; i++) {
         let propertyname = "slot" + i;
@@ -8,5 +7,13 @@ const gameboard = {};
     }
 })();
 
+function Player(name, target) {
+    this.name = name;
+    this.target = target;
+}
 
+const pOne = new Player("James", "X");
+const pTwo = new Player("Narumi", "O");
+
+console.log(pOne.name);
 console.log(gameboard);
